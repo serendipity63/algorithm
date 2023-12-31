529c7bf27a887fdad52ccafefcf4136b9f085719
Jisu <139774652+serendipity63@users.noreply.github.com>
2023-12-31 11:08:19 +0900

    [level unrated] Title: 로그인 성공?, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+	public String solution(String[] id_pw, String[][] db) {
+		String answer = "fail";
+		Map<String, String> hashMap = new HashMap<String, String>();
+		for (String[] strings : db) {
+			hashMap.put(strings[0], strings[1]);
+
+		}
+		if (hashMap.containsKey(id_pw[0])) {
+			if (hashMap.get(id_pw[0]).equals(id_pw[1])) {
+				return "login";
+			} else {
+				return "wrong pw";
+			}
+
+		} else {
+
+			return answer;
+		}
+
+	}
+}
+
