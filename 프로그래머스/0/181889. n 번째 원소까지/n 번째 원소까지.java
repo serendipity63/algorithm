4b51690decbff395547fa883067b56008d05d3e9
Jisu <139774652+serendipity63@users.noreply.github.com>
2024-02-11 13:58:49 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 69 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        
+answer= Arrays.copyOf(num_list, n);
+
+        return answer;
+    }
+}
