339ee692cf278fd61779dc529e81128150687a00
Jisu <139774652+serendipity63@users.noreply.github.com>
2024-04-18 17:23:19 +0900

    [Bronze III] Title: 곱셈, Time: 208 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int i= sc.nextInt();
+		int j= sc.nextInt();
+		
+        System.out.println(i * (j % 10));          
+        System.out.println(i * (j % 100 / 10));    
+        System.out.println(i * (j / 100));         
+        System.out.println(i * j);                 
+
+		
+		sc.close();
+		}
+	}
